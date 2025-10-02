95bb9b2c9c953767205630ce81774dfee46cd67d
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-02 23:30:44 +0530

    Create Program 16: Find the greatest of five numbers.c

---
+++ Program 16: Find the greatest of five numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main() {
+    int a, b, c, d, e;
+
+    printf("Enter five numbers: ");
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+
+    if (a >= b && a >= c && a >= d && a >= e) {
+        printf("The greatest number is %d\n", a);
+    } else if (b >= a && b >= c && b >= d && b >= e) {
+        printf("The greatest number is %d\n", b);
+    } else if (c >= a && c >= b && c >= d && c >= e) {
+        printf("The greatest number is %d\n", c);
+    } else if (d >= a && d >= b && d >= c && d >= e) {
+        printf("The greatest number is %d\n", d);
+    } else {
+        printf("The greatest number is %d\n", e);
+    }
+}
